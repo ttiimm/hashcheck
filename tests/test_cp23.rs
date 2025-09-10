f5dc12b3a8317ce3b15ae8ec46db6546675bda4c
Tim Likarish Ellis <likarish@gmail.com>
2025-09-10 17:30:42 -0400

    [Proptest] Add property based test for rolling cyclic poly 23 hash lib

--- tests/test_cp23.rs
+++ tests/test_cp23.rs
@@ -1,4 +1,5 @@
 use cyclic_poly_23::CyclicPoly32;
+use proptest::prelude::*;
 
 #[test]
 fn test_basic_32() {
@@ -17,4 +18,33 @@ fn test_basic_32_roll() {
     let next = hasher.rotate(data[0], data[4]);
     assert_ne!(previous, next);
     assert_eq!(next, 0xecf6e475);
+}
+
+// a vector of random u8 of size between 1-1000 and a window between 1-1000
+fn rolling_hash_strategy() -> impl Strategy<Value = (Vec<u8>, usize)> {
+    (
+        proptest::collection::vec(any::<u8>(), 1..1000),
+        1usize..1000usize,
+    )
+        .prop_filter("Window must be less than the data", |(data, window)| {
+            window <= &data.len()
+        })
+}
+
+proptest! {
+
+    #[test]
+    fn test_hashes_match_after_rolling((data, window) in rolling_hash_strategy()) {
+        let mut hasher = CyclicPoly32::new(window);
+        let mut rolling_hash_value = hasher.update(&data[0..window]);
+        for i in 1..=(data.len() - window) {
+            let leaving = data[i - 1];
+            let entering = data[i + window - 1];
+            rolling_hash_value = hasher.rotate(leaving, entering);
+            let mut tester = CyclicPoly32::new(window);
+            let expected = tester.update(&data[i..i+window]);
+            assert_eq!(rolling_hash_value, expected, "Hash mismatched at index {}", i);
+        }
+    }
+
 }
